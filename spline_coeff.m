0bc5c9e9353bb47242b53c32cea9261f6d1dc56d
Unknown <wiggelyjiggely@hotmail.com>
2018-05-04 16:17:31 +0200

    jn

--- spline_coeff.m
+++ spline_coeff.m
@@ -1,10 +1,10 @@
 function [c] = spline_coeff(x,f)
 n = length(x);
-c = zeros(n, n);
+c = zeros(n-1, n-1);
 for i = 1:n-1
-    for j = 1:n-1   
+    for j = 1 :n-1   
         [result] = evalBspline(j,3,f,x(i)); 
         c(i,j) = result;
     end
 end
-end
+end 
